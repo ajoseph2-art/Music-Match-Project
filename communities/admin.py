f223d0a9464a940983dfd4a83fd775343b8a3a3c
Aaron Joseph <aaronjoseph263@gmail.com>
2025-12-12 17:50:33 -0800

    Fix admin.py and create migration for removed models

--- communities/admin.py
+++ communities/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Community, ListeningParty, MusicMatch
+from .models import Community
 
 
 @admin.register(Community)
@@ -8,19 +8,4 @@ class CommunityAdmin(admin.ModelAdmin):
     list_filter = ['is_public', 'genre', 'created_at']
     search_fields = ['name', 'description', 'genre']
     filter_horizontal = ['members']
-
-
-@admin.register(ListeningParty)
-class ListeningPartyAdmin(admin.ModelAdmin):
-    list_display = ['name', 'community', 'host', 'start_time', 'is_active']
-    list_filter = ['is_active', 'start_time', 'created_at']
-    search_fields = ['name', 'community__name', 'host__username']
-    filter_horizontal = ['participants']
-
-
-@admin.register(MusicMatch)
-class MusicMatchAdmin(admin.ModelAdmin):
-    list_display = ['user1', 'user2', 'compatibility_score', 'calculated_at']
-    list_filter = ['calculated_at']
-    search_fields = ['user1__username', 'user2__username']
 
