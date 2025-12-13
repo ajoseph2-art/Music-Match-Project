673bceb525c517aee9be3844a360c0b0c8b2fbee
Aaron Joseph <aaronjoseph263@gmail.com>
2025-12-12 16:53:34 -0800

    Fix VM startup script using teacher template format

--- vm_startup_script.sh
+++ vm_startup_script.sh
@@ -9,10 +9,6 @@ DOCKER_IMAGE_TAG="latest"
 REG_HOST="${REGION}-docker.pkg.dev"
 DOCKER_IMAGE="${REG_HOST}/${GCP_PROJECT_ID}/${ARTIFACT_REPOSITORY_NAME}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
 
-# Spotify API Credentials
-SPOTIFY_CLIENT_ID="f3d8eeede32842ee8139616214cedd3f"
-SPOTIFY_CLIENT_SECRET="c5764d0ce4cc44d089787f604d7fa60c"
-
 # Docker needs a writable config on COS
 export DOCKER_CONFIG=/var/lib/docker-config
 mkdir -p "${DOCKER_CONFIG}"
@@ -20,17 +16,17 @@ mkdir -p "${DOCKER_CONFIG}"
 # Get an access token from the instance metadata server
 TOKEN="$(curl -s -H 'Metadata-Flavor: Google' \
 http://metadata/computeMetadata/v1/instance/service-accounts/default/token \
-|| awk -F'"' '/access_token/ {print $4}')"
+| awk -F'"' '/access_token/ {print $4}')"
 
 # Authenticate Docker to Artifact Registry
 echo "${TOKEN}" | docker login -u oauth2accesstoken --password-stdin "https://${REG_HOST}"
 
 # Pull and run container
 docker pull "${DOCKER_IMAGE}"
-docker rm -f ${DOCKER_IMAGE_NAME} || true
+docker rm -f final || true
 docker run -d --name ${DOCKER_IMAGE_NAME} --restart=always -p 80:80 \
     -e USE_SQLITE=True \
     -e DEBUG=False \
-    -e SPOTIFY_CLIENT_ID="${SPOTIFY_CLIENT_ID}" \
-    -e SPOTIFY_CLIENT_SECRET="${SPOTIFY_CLIENT_SECRET}" \
+    -e SPOTIFY_CLIENT_ID="f3d8eeede32842ee8139616214cedd3f" \
+    -e SPOTIFY_CLIENT_SECRET="c5764d0ce4cc44d089787f604d7fa60c" \
     "${DOCKER_IMAGE}"
